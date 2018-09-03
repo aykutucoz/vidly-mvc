5bd30907491b8b2162432556cdccc630803c2bb8
aykutucoz <aykutucoz@yahoo.com>
2018-09-03 10:25:20 +0300

    section-2

---
+++ Vidly/App_Start/RouteConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Vidly
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("MoviesById", "Movies/Random/{Id}",
+                new
+                {
+                    controller = "Movies",
+                    action="Random"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
