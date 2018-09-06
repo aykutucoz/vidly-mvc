23e9dec9e609565f12900025d128109386d61ca2
aykutucoz <aykutucoz@yahoo.com>
2018-09-06 14:12:19 +0300

    section 3 exercise 3

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,5 +9,10 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Genre { get; set; }
+        public DateTime RealeseDate { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int NumberInStock { get; set; }
+
     }
 }
