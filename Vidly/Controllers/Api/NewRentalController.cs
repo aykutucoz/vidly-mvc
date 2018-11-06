67d646089bc87a61df65bc50cd057be2ceefc72c
aykutucoz <aykutucoz@yahoo.com>
2018-11-06 14:37:41 +0300

    Section 10 Rental Table

---
+++ Vidly/Controllers/Api/NewRentalController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalController : ApiController
+    {   
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
